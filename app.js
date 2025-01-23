494667b6885cba42695155b13df3c2f7af119daa
CyberLime <togrul2006@inbox.ru>
2025-01-23 16:22:02 +0400

    e

---
+++ app.js
@@ -0,0 +1,24 @@
+import { readFile } from "fs/promises";
+
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  next();
+});
+
+app.get("/", async (req, res, next) => {
+  const data = await readFile("./data/data.json");
+  res.status(200).json({
+    countries: JSON.parse(data),
+    message: "Fetched data successfully!",
+  });
+});
+
+app.listen(3000);
